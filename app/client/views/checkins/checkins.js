3a5b9630f94e818a430df67f45605cae590a8fce
Tom G Varik <tgvarik@me.com>
2014-10-13 16:14:31 -0700

    sets up basic meteor app structure

---
+++ app/client/views/checkins/checkins.js
@@ -0,0 +1,17 @@
+/**
+ * Created by tom on 10/13/14.
+ */
+Template.checkins.helpers({
+  checkins: [
+    {
+    showName: 'Downton Abbey',
+    identifier: 'S05E03',
+    timestamp: '2014-10-12 00:00pm'
+  },
+    {
+      showName: 'Homeland',
+      identifier: 'S03E01',
+      timestamp: '2014-10-11 00:00pm'
+    }
+  ]
+});
